eefa7efd0418323b31888461fe3d3b4d736a5166
Kenneth Haagen Sørensen <Haagen86@gmail.com>
2025-11-05 21:42:36 +0100

    feat(app): define IStoragePresignClient outbound port

---
+++ src/MediaAccess.Application/Ports/Outbound/IStoragePresignClient.cs
@@ -0,0 +1,8 @@
+﻿using MediaAccess.Application.Ports.Outbound.Contracts;
+
+namespace MediaAccess.Application.Ports.Outbound;
+
+public interface IStoragePresignClient
+{
+  public Task<StoragePresignGetResponse> PresignGetAsync(StoragePresignGetRequest req, CancellationToken ct = default);
+}
